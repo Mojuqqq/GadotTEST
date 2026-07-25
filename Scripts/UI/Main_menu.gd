a0d9bbbe6cd6f7b4704cf6b5533b63a42d107078
AngellGirll <dina.pastushenko@mail.ru>
2026-07-25 22:40:26 +0300

    25,07 22,40
    Добавила меню старта

--- Scripts/UI/Main_menu.gd
+++ Scripts/UI/Main_menu.gd
@@ -4,8 +4,10 @@ func _ready():
 	# Показываем курсор (если скрыт)
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
-func _on_quit_button_pressed():
-	get_tree().quit()
 
-func _on_button_pressed() -> void:
+func _on_settings_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_new_game_button_pressed() -> void:
 	GameManager.start_game()
