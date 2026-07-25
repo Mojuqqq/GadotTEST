fd375fc1fbe386487305c3738da0851fc62aa2d8
AngellGirll <dina.pastushenko@mail.ru>
2026-07-25 23:47:40 +0300

    25,07 23,47
    Добавлены настройки

--- Scripts/UI/Main_menu.gd
+++ Scripts/UI/Main_menu.gd
@@ -1,12 +1,36 @@
 extends CanvasLayer
 
-func _ready():
-	# Показываем курсор (если скрыт)
-	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+@onready var settings_button: Button = (
+	$SettingsButton
+)
+
+@onready var settings_menu: SettingsMenu = (
+	$SettingsMenu
+)
+
+
+func _ready() -> void:
+	Input.set_mouse_mode(
+		Input.MOUSE_MODE_VISIBLE
+	)
+
+	if not settings_menu.closed.is_connected(
+		_on_settings_menu_closed
+	):
+		settings_menu.closed.connect(
+			_on_settings_menu_closed
+		)
+
+	settings_button.grab_focus()
 
 
 func _on_settings_button_pressed() -> void:
-	pass # Replace with function body.
+	settings_menu.open_menu()
+
+
+func _on_settings_menu_closed() -> void:
+	settings_button.grab_focus()
 
 
 func _on_new_game_button_pressed() -> void:
