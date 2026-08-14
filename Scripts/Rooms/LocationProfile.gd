9c9ea21662828de2accc6c058bd6789fd743256d
AngellGirll <dina.pastushenko@mail.ru>
2026-08-14 16:30:14 +0300

    14,08 16,30
    Генератор стен

--- Scripts/Rooms/LocationProfile.gd
+++ Scripts/Rooms/LocationProfile.gd
@@ -8,6 +8,13 @@ enum FloorType {
 	WOOD
 }
 
+enum WallType {
+	FARM,
+	BARN,
+	GARDEN
+}
+
+@export var wall_type: WallType = WallType.FARM
 
 @export_group("Location")
 
@@ -28,6 +35,38 @@ enum FloorType {
 
 @export var wall_tile_set: TileSet = null
 
+const WALL_TILES := {
+	LocationProfile.WallType.FARM: {
+		"top": Vector2i(0, 0),
+		"bottom": Vector2i(0, 2),
+		"left": Vector2i(0, 1),
+		"right": Vector2i(2, 1),
+		"top_left": Vector2i(0, 0),
+		"top_right": Vector2i(2, 0),
+		"bottom_left": Vector2i(0, 2),
+		"bottom_right": Vector2i(2, 2),
+	},
+
+	LocationProfile.WallType.BARN: {
+
+	},
+
+	LocationProfile.WallType.GARDEN: {
+
+	},
+}
+
+func _generate_walls(room, wall_type: LocationProfile.WallType) -> void:
+	var tiles: Dictionary = WALL_TILES[wall_type]
+
+	# дальше существующая генерация
+	# только вместо захардкоженного atlas_coords:
+
+	walls.set_cell(
+		position,
+		WALL_SOURCE_ID,
+		tiles["top"]
+	)
 
 @export_group("Doors")
 
---
+++ Scripts/Rooms/WallTheme.gd
@@ -0,0 +1,22 @@
+class_name WallTheme
+extends Resource
+
+@export var source_id: int = 0
+
+@export_group("Straight walls")
+@export var top: Vector2i
+@export var bottom: Vector2i
+@export var left: Vector2i
+@export var right: Vector2i
+
+@export_group("Corners")
+@export var top_left: Vector2i
+@export var top_right: Vector2i
+@export var bottom_left: Vector2i
+@export var bottom_right: Vector2i
+
+@export_group("Doors")
+@export var door_top: Vector2i
+@export var door_bottom: Vector2i
+@export var door_left: Vector2i
+@export var door_right: Vector2i
